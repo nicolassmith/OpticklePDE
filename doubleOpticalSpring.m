9c5fbc57ea602892f6f795a44cdb81b2ee06a09f
nicolassmith <nicolas.d.smith@gmail.com>
2012-07-19 12:29:12 -0400

    beginning to make double optical springs

--- doubleOpticalSpring.m
+++ doubleOpticalSpring.m
@@ -7,8 +7,12 @@ addpath('~/ligo/sim/Optickle/lib')
 addpath('/home/nicolas/git/optickle-tutorial/lib')
 
 % set laser powers
-Pc = 1;
+Pc = 4;
 Ps = Pc/20;
+
+% set detunings
+deltac = 0.5;
+deltas = 0;
 
 % this creates opt and par (setupPDE)
 par = paramPDE([],Pc);
@@ -18,11 +22,37 @@ par = paramPDE_LSC(par);
 c = 299792458;
 fGamma = c*par.IX.T/(8*pi*par.Length.Xarm);
 
-fSubcarrier = 3.5*fGamma;
+fSubcarrier = (deltas-deltac)*fGamma;
 
 par.PSL.vFrf = [par.PSL.vFrf;fSubcarrier;-fSubcarrier];
-par.PSL.vArf = [par.PSL.vArf;0;Ps];
+par.PSL.vArf = [par.PSL.vArf;sqrt(Ps);0];
 
 % make opt
 opt = optPDE(par);
 opt = probesPDE(opt,par);
+
+% set detuning
+pos = zeros(1,opt.Ndrive);
+
+fdelta = deltac*fGamma; %detuning
+
+pos(getDriveNum(opt,'EX')) = -fdelta/c*(opt.lambda*par.Length.Xarm); %detuning of EX in meters
+
+% tickle
+f = logspace(log10(500),4,500).';
+
+[fDC, sigDC, sigAC, mMech] = tickle(opt, pos, f);
+
+nEX = getDriveNum(opt,'EX');
+
+springTF = -getTF(mMech,nEX,nEX)./(f.^2);
+
+subplot(2,1,1)
+loglog(f,abs(springTF))
+xlim([min(f) max(f)])
+grid on
+subplot(2,1,2)
+semilogx(f,180/pi*angle(springTF))
+xlim([min(f) max(f)])
+ylim([-180 180])
+grid on
